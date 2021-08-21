4ac2d59b2e2731babe5534c81a48570f1d09e5dc
Xaver Pinero <xaver.pinero@gmail.com>
2021-08-20 20:10:41 -0400

    set up seeds

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,30 @@
 // STRETCH
+const cars = [
+  {
+    vin: '1234567890123',
+    make: "Toyota",
+    model: "Supra",
+    milage: 99000,
+    title: 'clean',
+    transmission: 'manual'
+  },
+  {
+    vin: '1234567890123',
+    make: "Toyota",
+    model: "Prius",
+    milage: 990000,
+    title: 'salvage',
+  },
+  {
+    vin: '1234567890123',
+    make: "Saleen",
+    model: "S7",
+    milage: 9000,
+  }
+]
+
+
+exports.seed = async function (knex) {
+  await knex('cars').truncate()
+  await knex('cars').insert(cars)
+}
