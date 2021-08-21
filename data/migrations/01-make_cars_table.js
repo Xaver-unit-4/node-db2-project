78be306da06306489ba495e1122c3cf4c432bbbd
Xaver Pinero <xaver.pinero@gmail.com>
2021-08-20 20:24:09 -0400

    migrate up schema

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,5 +1,14 @@
 exports.up = function (knex) {
   // DO YOUR MAGIC
+  return knex.schema.createTable('cars', table => {
+    table.increments()
+    table.string('vin', 17).notNullable().unique()
+    table.string('make', 50).notNullable()
+    table.string('model', 50).notNullable()
+    table.numeric('milage').unsigned().notNullable()
+    table.string('title', 50).defaultTo('unknown')
+    table.string('transmission', 50).defaultTo('unknown')
+  })
 };
 
 exports.down = function (knex) {
