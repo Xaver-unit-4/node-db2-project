66d3615edde43bf6becd6b3dc61afa1014504e49
Xaver Pinero <xaver.pinero@gmail.com>
2021-08-20 20:52:50 -0400

    server file

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,7 +1,7 @@
 // STRETCH
 const cars = [
   {
-    vin: '1234567890123',
+    vin: '12345678901234569',
     make: "Toyota",
     model: "Supra",
     milage: 99000,
@@ -9,14 +9,14 @@ const cars = [
     transmission: 'manual'
   },
   {
-    vin: '1234567890123',
+    vin: '12345678901234568',
     make: "Toyota",
     model: "Prius",
     milage: 990000,
     title: 'salvage',
   },
   {
-    vin: '1234567890123',
+    vin: '12345678901234567',
     make: "Saleen",
     model: "S7",
     milage: 9000,
